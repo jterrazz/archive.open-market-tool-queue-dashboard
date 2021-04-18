cbd69830da154b51a528cdeea3e63353b4f6526f
Jean-Baptiste Terrazzoni <jterrazz@icloud.com>
2021-04-18 17:01:02 +0200

    Init dashboard

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Queue = require('bull');
 const { setQueues, BullAdapter, router } = require('bull-board');
 const app = require('express')();
+import Redis from 'ioredis';
 
 const PORT = process.env.PORT || 8001;
 
@@ -8,13 +9,19 @@ const PORT = process.env.PORT || 8001;
  * Queues
  */
 
+const queueOptions = {
+    createClient: function () {
+        return new Redis(process.env.REDIS_URL);
+    },
+};
+
 const QueueIdentifier = {
     email: 'email',
     order: 'order',
 };
 
-const orderQueue = new Queue(QueueIdentifier.order, process.env.REDIS_URL);
-const emailQueue = new Queue(QueueIdentifier.email, process.env.REDIS_URL);
+const orderQueue = new Queue(QueueIdentifier.order, queueOptions);
+const emailQueue = new Queue(QueueIdentifier.email, queueOptions);
 
 setQueues([
     new BullAdapter(orderQueue),
